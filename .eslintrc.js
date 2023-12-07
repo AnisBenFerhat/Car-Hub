ee2e6d3fbc5883efaa6983a3caa2ca7edc2c1f18
AnisBenFerhat <anisbenferhat@gmail.com>
2023-12-07 18:14:17 +0100

    Setup: Init project

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
